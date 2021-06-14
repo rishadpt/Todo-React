82d7e4176bfa557402b93ee8b41ae51c4f3721d0
Rishad pt <rishadpt16@gmail.com>
2021-06-14 21:56:24 +0530

    TODO FUNCTIONS COMPLETED

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TodoApp from './component/TodoApp/TodoApp'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <TodoApp/>
-      </div>
-    )
-  }
-}
+export default ()=><TodoApp/>
+  
+
--- src/component/TodoApp/TodoApp.js
+++ src/component/TodoApp/TodoApp.js
@@ -2,39 +2,67 @@ import React, { Component } from 'react'
 import "./TodoApp.css"
 
 export default class TodoApp extends Component {
+
+
+    state={
+        input: "",
+        tasks: []
+    }
+
+
+    handleChange=(event)=>{
+        this.setState({
+            
+            input:event.target.value
+        })
+        
+
+    }
+    storeItems=(event)=>{
+        event.preventDefault()
+
+        const{ input }=this.state
+      
+        
+
+        this.setState({
+            tasks:[...this.state.tasks, input],
+            input:""
+        })
+       
+    }
+    deleteItem=(key)=>{
+      
+        
+
+    this.setState({
+        tasks:this.state.tasks.filter((data,index)=>index !== key)
+        
+    })
+
+    }
     render() {
+        const { input, tasks } = this.state
+     
         return (
-            <form className="todoapp_container">
+            <div className="todoapp_container" onSubmit={this.storeItems}>
          
-              <form className="input_section" >
+              <form className="input_section"  >
               <h1> TODOAPP</h1>
-                  <input className="input" type="text" placeholder="Enter The Task" />
+                  <input className="input" type="text" placeholder="Enter The Task" value={input} onChange={this.handleChange}/>
                   
               </form>
               <ul>
+                  
+                  {tasks.map((data, index)=>(
 
-                  <li>
-                      TASKS
-                     <i className="fas fa-trash-alt"></i>
-                  </li>
-                  <li>
-                      TASKS
-                     <i className="fas fa-trash-alt"></i>
-                  </li>
-                  <li>
-                      TASKS
-                     <i className="fas fa-trash-alt"></i>
-                  </li>
-                  <li>
-                      TASKS
-                     <i className="fas fa-trash-alt"></i>
-                  </li>
-                  <li>
-                      TASKS
-                     <i className="fas fa-trash-alt"></i>
-                  </li>
+                
+
+                      <li key={index}>{data}<i className="fas fa-trash-alt" onClick={()=>this.deleteItem(index)}></i></li>
+                  ))}
+                 
               </ul>
-            </form>
+            </div>
         )
     }
 }
